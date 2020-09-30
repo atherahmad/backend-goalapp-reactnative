2059153d5d1469803bcd61e1561811e2666a1961
Ather Ahmad <atherahmad49@yahoo.com>
2020-09-30 22:54:02 +0200

    dot env

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ require("dotenv").config();
 
 connectDB();
 
-app.use("/api/goals", require("./router/goals"))
-    
+app.use("/api/goals", require("./router/goals"));
 
 app.listen(process.env.PORT, () =>
   console.log(`Server is running at port ${process.env.PORT}`)
